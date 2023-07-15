94c76db5337f8aaf4f54f6eff9732e32960a1f13
levch <levchenko.d.a1998@gmail.com>
2023-07-15 23:01:50 +0600

    -

--- deps.py
+++ deps.py
@@ -1,51 +1,55 @@
 import env
 
-VIBER_MODE = env.get('PLATFORM') == "viber"
+VIBER_MODE = env.get("PLATFORM") == "viber"
 
 if VIBER_MODE:
     from viber import (
-        run,
-        bot,
-        app,
         CallbackButton,
-        UrlButton,
-        InlineKeyboard,
-        ReplyKeyboard,
+        CancelHandler,
         ContactRequestButton,
+        Dispatcher,
+        FSMContext,
+        InlineKeyboard,
+        Message,
+        ReplyKeyboard,
         State,
         StatesGroup,
-        Dispatcher,
-        Message,
-        reply,
-        CancelHandler,
-        dp,
-        FSMContext,
+        UrlButton,
+        app,
         ask,
+        bot,
+        dp,
         get_photo_url,
+        reply,
+        run,
+    )
+    from viber import (
         CallbackQuery as Query,
     )
 else:
     from botty import (
-        run,
-        bot,
-        app,
         CallbackButton,
-        UrlButton,
-        InlineKeyboard,
-        ReplyKeyboard,
+        CancelHandler,
         ContactRequestButton,
+        Dispatcher,
+        FSMContext,
+        InlineKeyboard,
+        Message,
+        Query,
+        ReplyKeyboard,
         State,
         StatesGroup,
-        Dispatcher,
-        Message,
-        reply,
-        CancelHandler,
-        dp,
-        FSMContext,
+        UrlButton,
+        app,
         ask,
+        bot,
+        dp,
         get_photo_url,
-        Query,
+        reply,
+        run,
     )
+
+    bot.disable_web_page_preview = False
 
 Event = Message | Query
 
